9a278ae0fa6956deeec4098a31a73ee939869979
Priyam Srivastava <108716553+Priyam-Srivastava2001@users.noreply.github.com>
2022-07-06 12:09:27 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\chatbot-covid19-bot-9031-Web-Deploy.pubxml -p:Password=omlx91Qk6bY5pi8nodfTqKfnF9l4nD6xCmd6MQm85KQRW9nHhF8EBcFieXkr
+
